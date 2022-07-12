ccca209f747d673ef401359d66b48da98874d8ad
Estuardo Villeda <Estuardovilleda2001@gmail.com>
2022-07-11 20:35:59 -0600

    remove unused routes

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/seguimiento', require('./routes/api/seguimiento'));
+
+
+
 app.use('/api/orgs', require('./routes/api/orgs'));
 
 
